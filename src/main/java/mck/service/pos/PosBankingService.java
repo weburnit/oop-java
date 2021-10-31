cf761e888ae2ea09ee796fb98acde008d553e0bc
bindzo <hpesc1133@gmail.com>
2021-10-31 23:35:18 +0700

    Implement PosBankingService

--- src/main/java/mck/service/pos/PosBankingService.java
+++ src/main/java/mck/service/pos/PosBankingService.java
@@ -13,5 +13,44 @@ import mck.spi.DepositCredit;
 import mck.spi.TransferCredit;
 
 public class PosBankingService implements DigitalBankingService {
+    DepositCredit depositCredit;
+    TransferCredit transferCredit;
+    DigitalBankingService provider;
 
+    PosBankingService ( DepositCredit depositCredit, TransferCredit transferCredit, DigitalBankingService provider) {
+        this.depositCredit = depositCredit;
+        this.transferCredit = transferCredit;
+        this.provider = provider;
+    }
+
+    @Override
+    public DepositResponse DepositFromBank(int currentUser, DepositFromBankRequest request) {
+        return this.depositCredit.DepositFromBank(currentUser, request);
+    }
+
+    @Override
+    public DepositResponse DepositFromMobileCard(int currentUser, DepositFromMobileRequest request) {
+        return this.depositCredit.DepositFromMobileCard(currentUser, request);
+    }
+
+    @Override
+    public Error RegisterAccount(RegisterAccountRequest request) {
+        //Implement its own RegisterAccount function
+        return null;
+    }
+
+    @Override
+    public Error ActiveAccount(ActiveAccountRequest request) {
+        return this.provider.ActiveAccount(request);
+    }
+
+    @Override
+    public TransferMoneyResponse TransferMoneyByUserID(int senderID, MoneyByUserIDRequest request) {
+        return this.transferCredit.TransferMoneyByUserID(senderID, request);
+    }
+
+    @Override
+    public TransferMoneyResponse TransferMoneyByMobileNumber(int senderID, TransferMoneyByMobileNumberRequest request) {
+        return this.transferCredit.TransferMoneyByMobileNumber(senderID, request);
+    }
 }
