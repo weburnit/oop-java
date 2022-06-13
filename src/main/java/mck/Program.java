420035b476d1e7cf63c620c5b2438cba43e08135
Thai <huuthai1998@gmail.com>
2022-06-13 15:16:25 +0700

    changed to mck

--- src/main/java/mck/Program.java
+++ src/main/java/mck/Program.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Program {
     public static void main(String[] args) throws Exception {
-        System.out.print("Hello World - Thai");
+        System.out.print("Hello World - McK");
         SpringApplication.run(Program.class, args);
     }
 }
